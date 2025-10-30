6a7e48725d7b9d9d5587668774635c56d50023a6
wiyenya <kategrigoreva2003@gmail.com>
2025-10-30 20:08:57 +0200

    Add empty migration

---
+++ parser/migrations/0001_initial.py
@@ -0,0 +1,15 @@
+# Generated manually - tables already exist in database
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+    ]
+
